6f6588d5be6aef970dbfe15f9dad90dbfebb2fb2
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2023-11-04 17:24:46 +0300

    backend done

--- index.js
+++ index.js
@@ -3,96 +3,29 @@ import express from 'express';
 import { PORT, dbUrl } from './config.js';
 import mongoose from 'mongoose';
 import {Task} from './models/todoModels.js'
+import tasksRoutes from "./routes/tasksRoutes.js"
+import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Access your environment variables
+const mongoDBConnectionString = process.env.MONGODB_CONNECTION_STRING;
+
 const app = express();
 app.use(express.json())
 
-
+app.use(cors({
+  origin:'http://localhost:3000',
+  methods:['GET','POST','PUT','DELETE'],
+  allowedHeaders:['Content-Type'],
+}))
 app.get('/', (req, res) => {
   console.log(req);
   res.sendStatus(234);
 });
-// get (/tasks)
-app.get('/tasks',async (req, res) => {
-  try{
-const tasks = await Task.find({})
-return res.status(200).json({count:tasks.length,
-  data:tasks});
-  }
-  catch(error){
-    console.log(error.message);
-    res.sendStatus(500).send({message: error.message});
-  }
 
-});
-
- // get id (/tasks/id)
-app.get('/tasks/:id',async (req, res) => {
-  try{
-    const {id} = req.params
-const task = await Task.findById(id)
-return res.status(200).json(task);
-  }
-  catch(error){
-    console.log(error.message);
-    res.sendStatus(500).send({message: error.message});
-  }
-
-});
-
-
-// post  (/tasks)
-app.post('/tasks',async (req, res) => {
-  try{
-    if(!req.body.des|| !req.body.time ){
-     return res.status(400).send({message : 'send all fields'});
-    }
-const newTask= {
-  des : req.body.des,
-  time: req.body.time,
-  completed : req.body.completed,
-}
-const task = await Task.create(newTask)
-return res.status(201).send(task)
-  } 
-  catch(error){
-    console.log(error);
-    res.sendStatus(500)
-  }
-})
-
-// UPDATE   tasks/id
-app.put('/tasks/:id',async (req, res) => {
-  try{
-    const {id} = req.params
-    const result = await Task.findByIdAndUpdate(id, req.body)  
-    if(!result){return res.status(404).json({message:"Task not found"})}  
-    return res.status(200).json({message:"updated successfully"})
-  }
-  catch(error){
-    console.log(error.message);
-    res.sendStatus(500).send({message: error.message});
-  }
-
-});
-
-
-
-// DELETE  tasks/id
-app.delete('/tasks/:id',async (req, res) => {
-  try{
-    const {id} = req.params
-    const result = await Task.findByIdAndDelete(id, req.body)  
-    if(!result){return res.status(404).json({message:"Task not found"})}  
-    return res.status(200).json({message:"deleted successfully"})
-  }
-  catch(error){
-    console.log(error.message);
-    res.sendStatus(500).send({message: error.message});
-  }
-
-});
-
-mongoose.connect(dbUrl)
+app.use('/tasks',tasksRoutes)
+mongoose.connect(mongoDBConnectionString)
 .then(()=>{
   console.log('connected')
   app.listen(PORT, () => {
---
+++ routes/tasksRoutes.js
@@ -0,0 +1,91 @@
+import  express  from "express";
+import {Task} from '../models/todoModels.js'
+const router = express.Router()
+
+
+
+
+
+
+// get (/tasks)
+router.get('/',async (req, res) => {
+    try{
+  const tasks = await Task.find({})
+  return res.status(200).json({count:tasks.length,
+    data:tasks});
+    }
+    catch(error){
+      console.log(error.message);
+      res.sendStatus(500).send({message: error.message});
+    }
+  
+  });
+  
+   // get id (/tasks/id)
+   router.get('/:id',async (req, res) => {
+    try{
+      const {id} = req.params
+  const task = await Task.findById(id)
+  return res.status(200).json(task);
+    }
+    catch(error){
+      console.log(error.message);
+      res.sendStatus(500).send({message: error.message});
+    }
+  
+  });
+  
+  
+  // post  (/tasks)
+  router.post('/',async (req, res) => {
+    try{
+      if(!req.body.des|| !req.body.time ){
+       return res.status(400).send({message : 'send all fields'});
+      }
+  const newTask= {
+    des : req.body.des,
+    time: req.body.time,
+    completed : req.body.completed,
+  }
+  const task = await Task.create(newTask)
+  return res.status(201).send(task)
+    } 
+    catch(error){
+      console.log(error);
+      res.sendStatus(500)
+    }
+  })
+  
+  // UPDATE   tasks/id
+  router.put('/:id',async (req, res) => {
+    try{
+      const {id} = req.params
+      const result = await Task.findByIdAndUpdate(id, req.body)  
+      if(!result){return res.status(404).json({message:"Task not found"})}  
+      return res.status(200).json({message:"updated successfully"})
+    }
+    catch(error){
+      console.log(error.message);
+      res.sendStatus(500).send({message: error.message});
+    }
+  
+  });
+  
+  
+  
+  // DELETE  tasks/id
+  router.delete('/:id',async (req, res) => {
+    try{
+      const {id} = req.params
+      const result = await Task.findByIdAndDelete(id, req.body)  
+      if(!result){return res.status(404).json({message:"Task not found"})}  
+      return res.status(200).json({message:"deleted successfully"})
+    }
+    catch(error){
+      console.log(error.message);
+      res.sendStatus(500).send({message: error.message});
+    }
+  
+  });
+
+  export default router;
