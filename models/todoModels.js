2f3284daec115de83c5404e58ca1676203847260
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2024-03-04 20:28:54 +0300

    new branch

--- models/todoModels.js
+++ models/todoModels.js
@@ -1,20 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const taskSchema=mongoose.Schema({
-  des:{
-  type:String,
-  required:true
+const taskSchema = mongoose.Schema({
+  des: {
+    type: String,
+    required: true
   },
-   time:{
-    type:Number,
-    required:true
+  time: {
+    type: Number,
+    required: true
   },
-   completed:{
-    type:Boolean,
-    required:true
-  },  
-}
-,
-{timestamps:true}
-)
-export const Task = mongoose.model('Task',taskSchema);
+  completed: {
+    type: Boolean,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+},
+{
+  timestamps: true
+});
+
+export const Task = mongoose.model('Task', taskSchema);
